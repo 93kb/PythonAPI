6e16042e8b288eb024e368d3d324ba43675a1cb9
sana kusakabe <s.kusakabe@peach-tech.co.jp>
2025-03-07 16:45:52 +0900

    GeminiAIと通信

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests,json
+from fastapi import FastAPI
+app = FastAPI()
+
+@app.get("/zipcode")
+async def ZipCode(zipcode:int = 0):
+    url = f'https://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}'
+    r = requests.get(url)
+    print(r.text)
+    return {"message":   json.loads(r.text)}
+
+"""
+とりあえず　https://qiita.com/thithi7110/items/c1b01798e69ddc31206b
+のままに作って実行したけど動かない　なんで？
+
+"""
